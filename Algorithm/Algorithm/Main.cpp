98c73acfbeb7642959ebe47232be027384706cce
hexinping <hexinping456@126.com>
2019-02-20 08:20:15 +0800

    算法练习1，利用map降低时间复杂度

--- Algorithm/Algorithm/Main.cpp
+++ Algorithm/Algorithm/Main.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 
+#include "example1.h"
+
+
 
 int main(int argc, char* argv[])
 {
+
+	//example1
+	vector<int> ilist = { 2, 7, 11, 15 };
+	int target = 9;
+	vector<int> result;
+	result = twoSum2(ilist,target);
+
 	system("pause");
 
 	return 0;
