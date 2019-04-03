8883f612e77f47f22478d38eecdc3ffbdfb519f0
hexinping <hexinping456@126.com>
2019-04-03 14:59:24 +0800

    887

--- Algorithm/Algorithm/example887.h
+++ Algorithm/Algorithm/example887.h
@@ -47,3 +47,23 @@ int superEggDrop(int K, int N) {
 
 
 */
+
+/*
+int superEggDrop(int K, int N) {
+
+	vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+	int m = 0;
+	while (dp[m][K] < N)
+	{
+		m++;
+		for (int k = 1; k <= K; ++k)
+			dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+	}
+	return m;
+
+}
+
+	
+
+
+*/
