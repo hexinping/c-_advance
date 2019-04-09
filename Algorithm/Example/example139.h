a6c704fcfa2b064056c36ce1995777aedba9ce68
hexinping <hexinping456@126.com>
2019-04-09 16:16:45 +0800

    动态规划解决子串算法

--- Algorithm/Example/example139.h
+++ Algorithm/Example/example139.h
@@ -110,3 +110,52 @@ string removeEqualStr(string str, string word)
 	 }
 	 return result;
  }
+
+ /*
+
+笔记】动态规划。dp[i]表示字符串s的前i个字符能否拆分成wordDict。
+
+8ms
+
+bool wordBreak(string s, vector<string>& wordDict) {
+    vector<bool> dp(s.size()+1, false);
+    unordered_set<string> m(wordDict.begin(), wordDict.end());
+    dp[0] = true;
+    for (int i = 1; i <= s.size(); ++i){
+        for (int j = 0; j < i; ++j){
+            if (dp[j] && m.find(s.substr(j, i-j)) != m.end()){
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.size()];
+}
+【优化】对于以上代码可以优化。每次并不需要从s[0]开始搜索。因为wordDict中的字符串长度是有限的。只需要从i-maxWordLength开始搜索就可以了。
+
+0ms
+
+bool wordBreak(string s, vector<string>& wordDict) {
+    vector<bool> dp(s.size()+1, false);
+    unordered_set<string> m(wordDict.begin(), wordDict.end());
+    dp[0] = true;
+    //获取最长字符串长度
+    int maxWordLength = 0;
+    for (int i = 0; i < wordDict.size(); ++i){
+        maxWordLength = std::max(maxWordLength, (int)wordDict[i].size());
+    }
+    for (int i = 1; i <= s.size(); ++i){
+        for (int j = std::max(i-maxWordLength, 0); j < i; ++j){
+            if (dp[j] && m.find(s.substr(j, i-j)) != m.end()){
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[s.size()];
+}
+
+
+
+
+ */
