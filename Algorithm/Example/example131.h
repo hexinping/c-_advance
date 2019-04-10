47fc1a732045c69e892442bd24801ed04aaa31a5
hexinping <hexinping456@126.com>
2019-04-10 11:26:20 +0800

    DFS回溯

--- Algorithm/Example/example131.h
+++ Algorithm/Example/example131.h
@@ -24,14 +24,57 @@ int singleNumber(vector<int>& nums) {
 
 */
 
+/*
+	C++，dfs，回溯剪枝。
+
+	这种从前往后都是同一个规律的题，就要想到递归了。
+
+	对于字符串S的当前子串[left, right]，要找到其中所有的回文串组合，要完成如下工作：
+
+		1 定义变量i从left到right进行分割，得到子串S[left, i]；
+		2 如果子串S[left, i]是回文串，则问题发生变化；
+		3 新问题：对于字符串S的当前子串[i + 1, right]，找到其中所有的回文串组合
+		…
+		如果left >= right，则说明当前路径下的回文串组合生成结束
+
+*/
+
 #include <iostream>
 using namespace std;
 
 #include <vector>
 #include <map>
 
-int singleNumber_131(vector<int>& nums)
+vector<vector<string>> partition(string s) 
+{
+        vector<vector<string>> ans;
+        if (s.empty()) return ans;
+        vector<string> temp;
+        dfs(ans, s, temp, 0);
+        return ans;
+ }
+    
+ void dfs(vector<vector<string>> &ans, string &s, vector<string> &temp, int left) 
  {
+    if (left >= s.length())
+        ans.push_back(temp);
+    else {
+        for (int right = left; right < s.length(); right++) {
+            string ts = s.substr(left, right - left + 1);
+            if (is_pali(s, left, right)) {
+                temp.push_back(ts);
+                dfs(ans, s, temp, right + 1);
+                temp.pop_back();
+            }
+        }
+    }
+ }
 
-	 return 0;
+//判断是否是回文串
+bool is_pali(string &s, int left, int right) {
+    if (left == right || left > right) 
+        return true;
+    if (s[left] == s[right])
+        return is_pali(s, left + 1, right - 1);
+    else return false;
 }
